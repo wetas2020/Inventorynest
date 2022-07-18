8f80cc1c61e52c94727094b3b73bcc634ed1b4ee
Saad El Gabouri <elgabouri.saad@gmail.com>
2022-07-18 12:15:25 +0100

    Setup Jwt

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
 });
